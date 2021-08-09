82b5928251fd66f7d6eec906bd309e4af46b715a
Papa Daco <papadaco@PapaDaco.local>
2021-08-08 21:23:01 -0400

    Re-init of CFADB's Flash Chat app for Android v.2

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,88 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import './chat_screen.dart';
+import '../constants.dart';
+import '../widgets/padded_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (value) {
+                email = value;
+              },
+              decoration: kTextFieldDeocration.copyWith(
+                hintText: 'Enter your email',
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
+              onChanged: (value) {
+                password = value;
+              },
+              decoration: kTextFieldDeocration.copyWith(
+                hintText: 'Enter your password',
+              ),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            PaddedButton(
+              color: Colors.lightBlueAccent,
+              text: 'Log In',
+              onPressed: () async {
+                try {
+                  final user = await _auth.signInWithEmailAndPassword(
+                    email: email,
+                    password: password,
+                  );
+                  if (user != null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
